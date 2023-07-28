de9b166e951ab41aa516b8df1071e014bcd9405b
tejasnswamy <tejas.narayanaswamy@clevertap.com>
2023-07-28 18:43:46 +0530

    Initial Commit

---
+++ Tejas_IOS_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tejas_IOS_Demo
+//
+//  Created by Tejas Narayanaswamy on 28/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
